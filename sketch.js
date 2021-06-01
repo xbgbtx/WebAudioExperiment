39b1cbf0b5a757c690ae8dde748a6d5c4145b328
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-06-01 15:56:01 +0100

    Mic music visualizer

--- sketch.js
+++ sketch.js
@@ -2,9 +2,12 @@ let analyser;
 let data;
 let audioCtx;
 
+const num_boxes = 64;
+const fftSize = 2048;
+
 function setup ()
 {
-    createCanvas ( 800, 600 );
+    createCanvas ( 600, 600 );
 
     let button = createButton ( 'Listen' );
     button.position ( 0, 0 );
@@ -26,37 +29,78 @@ async function get_user_speakers ()
 
 
     analyser = audioCtx.createAnalyser ();
-    analyser.fftSize = 2048;
+    analyser.fftSize = fftSize;
     mic.connect ( analyser );
 
     data = new Uint8Array ( analyser.frequencyBinCount );
+
+}
+
+function amps ( data )
+{
+    let a = [];
+
+    let chunk = Math.floor(data.length * 0.5);
+
+    data = data.slice ( 0, chunk );
+
+    let span = data.length / num_boxes;
+
+    for ( let i = 0; i < num_boxes; i++ ) {
+        let x = 0;
+        
+        let start = span * i;
+        let end = start + span;
+
+        for ( let j = start; j < end; j++ )
+            x += data [ start + j ];
+
+        a.push ( x / span );
+    }
+
+    return a;
 }
 
 function draw ()
 {
-    if ( analyser == null )
-        return;
+    background ( 16, 23, 17, 120 );
 
-    analyser.getByteFrequencyData ( data );
+    let rect_sizes;
 
-    let peak = Math.max ( ...data );
+    if ( analyser == null ) {
+        rect_sizes = [];
 
-    let t = millis()/1000;
-    let a = TWO_PI * t/2;
+        for ( let i = 0; i<num_boxes; i++ )
+            rect_sizes.push ( 0.25 );
+    }
+    else {
+        analyser.getByteFrequencyData ( data );
+        rect_sizes = amps ( data ).map ( a => map ( a, 0, 255, 0, 1 ) );
+    }
 
-    background ( 50 );
+    let grid_w = width/sqrt(num_boxes);
+    let grid_h = height/sqrt(num_boxes);
     
     stroke ( 255 );
     noFill ();
 
-    rectMode ( CENTER );
-    push ();
+    let t = millis()/60;
 
-    translate ( width/2, height/2 );
-    rotate ( a );
+    let idx_offset = Math.floor ( t ) % num_boxes;
 
-    rect ( 0, 0, peak, width/10 );
+    for ( let i = 0; i<sqrt(num_boxes); i++ )
+    {
+        for ( let j = 0; j<sqrt(num_boxes); j++ )
+        {
+            let idx = (sqrt(num_boxes) * i + j + idx_offset)%num_boxes;
+            let s = rect_sizes [ idx ];
+            let w = grid_w * s;
+            let h = grid_h * s;
 
-    pop ();
+            let x = ( grid_w * i ) + ( grid_w / 2 ) - w/2;
+            let y = ( grid_h * j ) + ( grid_h / 2 ) - h/2;
+            rect ( x, y, w, h );
+        }
+    }
 }
 
